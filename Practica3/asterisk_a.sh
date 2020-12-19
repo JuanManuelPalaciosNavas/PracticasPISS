eaa692ccb0e4d88a7bf0fc355755976f307c0602
JuanManuelPalaciosNavas <juanma.palacios@hotmail.es>
2020-12-19 11:48:05 +0100

    Backup practica 3

---
+++ Practica3/asterisk_a.sh
@@ -0,0 +1,5 @@
+
+cp /vagrant/a_conf/sip.conf /etc/asterisk/
+cp /vagrant/a_conf/extensions.conf /etc/asterisk/
+
+service asterisk restart
---
+++ Practica3/asterisk_b.sh
@@ -0,0 +1,5 @@
+
+cp /vagrant/b_conf/sip.conf /etc/asterisk/
+cp /vagrant/b_conf/extensions.conf /etc/asterisk/
+
+service asterisk restart
